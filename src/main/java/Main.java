ba1a32d72a1accdd38d53c8ede0b3551a81eb5b8
tardi <tardieusamuel@gmail.com>
2022-10-10 00:41:51 +0200

    maj

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    public void main(String[] args){
+    public void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanFile = new Scanner(new File("example.txt"));
+        String line = scanFile.nextLine();
+
+        while (scanFile.hasNext()){
+            line = scanFile.nextLine();
+
+        }
+        scanFile.close();
+
 
         List<Sommet> pile = new ArrayList<>();
         List<Sommet> sommetMarque = new ArrayList<>();
@@ -16,6 +29,9 @@ public class Main {
             pile.remove(cur);
             cur.getEnfants().sort(Comparator.comparing(Sommet::getNum));
             pile.addAll(cur.getEnfants());
+        }
+        for(Sommet mar : sommetMarque){
+            System.out.println(mar.getNum());
         }
     }
 }
