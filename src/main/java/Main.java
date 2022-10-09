bffe3d7a7d7ac6b8d8f02c6c1910a4c1f61f401c
tardi <tardieusamuel@gmail.com>
2022-10-10 01:13:47 +0200

    fini (normalement)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,25 +10,44 @@ public class Main {
 
         Scanner scanFile = new Scanner(new File("example.txt"));
         String line = scanFile.nextLine();
-
+        Sommet first = new Sommet(Integer.parseInt(line.substring(0, 1)));
+        List<Sommet> allPastSommet = new ArrayList<>();
+        allPastSommet.add(first);
+        List<Sommet> allCurrentSommet = new ArrayList<>();
         while (scanFile.hasNext()){
             line = scanFile.nextLine();
-
+            for(int i = 0; i<line.length(); i++){
+                int compteurLecture = 0;
+                int compteurEnfant = 0;
+                int parseInt = Integer.parseInt(line.substring(compteurLecture, i));
+                if(line.charAt(i) == ','){
+                    Sommet s = new Sommet(parseInt);
+                    allCurrentSommet.add(s);
+                    allPastSommet.get(compteurEnfant).addEnfants(s);
+                }
+                if(line.charAt(i) == ';'){
+                    Sommet s = new Sommet(parseInt);
+                    allCurrentSommet.add(s);
+                    allPastSommet.get(compteurEnfant).addEnfants(s);
+                    compteurEnfant++;
+                }
+            }
+            allPastSommet = allCurrentSommet;
+            allCurrentSommet.clear();
         }
         scanFile.close();
 
 
         List<Sommet> pile = new ArrayList<>();
         List<Sommet> sommetMarque = new ArrayList<>();
-        Sommet s = new Sommet(1);
-        pile.add(s);
+        pile.add(first);
         while(!pile.isEmpty()){
-            Sommet cur = pile.get(pile.size() - 1);
-            cur.marque();
-            sommetMarque.add(cur);
-            pile.remove(cur);
-            cur.getEnfants().sort(Comparator.comparing(Sommet::getNum));
-            pile.addAll(cur.getEnfants());
+            Sommet current = pile.get(pile.size() - 1);
+            current.marque();
+            sommetMarque.add(current);
+            pile.remove(current);
+            current.getEnfants().sort(Comparator.comparing(Sommet::getNum));
+            pile.addAll(current.getEnfants());
         }
         for(Sommet mar : sommetMarque){
             System.out.println(mar.getNum());
