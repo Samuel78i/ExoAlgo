32a3698126015d4cdaf87d5f5d32ca0d619ca78c
Zorball24 <62960455+Zorball24@users.noreply.github.com>
2022-10-10 17:27:38 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
-public class MainBis {
+public class Main {
     public static void main(String[] args) {
     	List<Sommet> sommets;
     	
