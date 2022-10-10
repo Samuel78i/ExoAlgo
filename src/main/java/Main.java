735e936aa4153e11a12bc244232773b7664abc01
Zorball24 <62960455+Zorball24@users.noreply.github.com>
2022-10-10 17:26:39 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,63 +1,97 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) throws FileNotFoundException, URISyntaxException {
-        Scanner scanFile;
-        URL resource = Main.class.getClassLoader().getResource("arbre.txt");
-        if (resource == null) {
-            throw new IllegalArgumentException("file not found!");
-        } else {
-            scanFile = new Scanner(new File(resource.toURI()));
+public class MainBis {
+    public static void main(String[] args) {
+    	List<Sommet> sommets;
+    	
+    	try {
+			sommets = lecture("arbre.txt");
+		} catch (IOException e) {
+			System.err.println("Erreur dans l'ouverture du fichier.");
+			return;
+		}
+    	List<Sommet> listFinal = new ArrayList<Sommet>();
+    	List<Sommet> pile = new ArrayList<Sommet>();
+    	
+        for (Sommet sommet : sommets) {
+			if (!sommet.isMarque()) {
+		        pile.add(sommet);
+		        while(!pile.isEmpty()){
+		            Sommet current = pile.get(pile.size() - 1);
+		            if (current.isMarque()) {
+		            	pile.remove(current);
+		            	continue;
+		            }
+		            
+		            current.marque();
+		            listFinal.add(current);
+		            pile.remove(current);
+		            
+		            current.getEnfants().sort(Comparator.comparing(Sommet::getNum).reversed());
+		            for (Sommet s : current.getEnfants()) {
+						if (!s.isMarque()) {
+							pile.add(s);
+						}
+					}
+		        }
+			}
+		}
+        for (Sommet sommet : listFinal) {
+			System.out.print(sommet + " ");
+		}
+        
+        
+    }
+    
+    private static List<Sommet> lecture(String chemin) throws IOException {
+    	BufferedReader br = new BufferedReader(new FileReader(chemin));
+        
+        List<Sommet> sommets = new ArrayList<Sommet>();
+        
+        String line;
+        while ((line = br.readLine()) != null) {
+        	Scanner sc = new Scanner(line);
+        	sc.useDelimiter(" ");
+        	
+        	int num = Integer.parseInt(sc.next());
+        	
+        	
+        	Sommet s = getSommet(sommets, num);
+        	if (s == null) {
+        		s = new Sommet(num);
+        		sommets.add(s);
+        	}
+        	
+        	while(sc.hasNext()) {
+        		num = Integer.parseInt(sc.next());
+        		Sommet enfant = getSommet(sommets, num);
+            	if (enfant == null) {
+            		enfant = new Sommet(num);
+            		sommets.add(enfant);
+            	}
+        		
+            	s.addEnfants(enfant);
+        	}
+        	sc.close();
         }
-        String line = scanFile.nextLine();
-        Sommet first = new Sommet(Integer.parseInt(line.substring(0, 1)));
-        List<Sommet> allPastSommet = new ArrayList<>();
-        allPastSommet.add(first);
-        List<Sommet> allCurrentSommet = new ArrayList<>();
-        while (scanFile.hasNext()){
-            line = scanFile.nextLine();
-            for(int i = 0; i<line.length(); i++){
-                int compteurLecture = 0;
-                int compteurEnfant = 0;
-                int parseInt = Integer.parseInt(line.substring(compteurLecture, i));
-                if(line.charAt(i) == ','){
-                    Sommet s = new Sommet(parseInt);
-                    allCurrentSommet.add(s);
-                    allPastSommet.get(compteurEnfant).addEnfants(s);
-                }
-                if(line.charAt(i) == ';'){
-                    Sommet s = new Sommet(parseInt);
-                    allCurrentSommet.add(s);
-                    allPastSommet.get(compteurEnfant).addEnfants(s);
-                    compteurEnfant++;
-                }
-            }
-            allPastSommet = allCurrentSommet;
-            allCurrentSommet.clear();
-        }
-        scanFile.close();
 
-
-        List<Sommet> pile = new ArrayList<>();
-        List<Sommet> sommetMarque = new ArrayList<>();
-        pile.add(first);
-        while(!pile.isEmpty()){
-            Sommet current = pile.get(pile.size() - 1);
-            current.marque();
-            sommetMarque.add(current);
-            pile.remove(current);
-            current.getEnfants().sort(Comparator.comparing(Sommet::getNum));
-            pile.addAll(current.getEnfants());
-        }
-        for(Sommet mar : sommetMarque){
-            System.out.println(mar.getNum());
-        }
+        br.close();
+        
+        return sommets;
+    }
+    
+    
+    private static Sommet getSommet(List<Sommet> sommmets, int numSommet) {
+    	for (Sommet sommet : sommmets) {
+			if (sommet.getNum() == numSommet)
+				return sommet;
+		}
+    	return null;
     }
 }
